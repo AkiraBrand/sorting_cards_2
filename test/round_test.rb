be3aca67fad3ca7ce0373d4e78bad07d3dbef120
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-19 17:50:11 -0600

    add round test and class

---
+++ test/round_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/guess'
+require './lib/deck'
+require './lib/round'
+require 'pry'
+
+class RoundTest < Minitest::Test
+
+  def test_it_exists
+   card_1 = Card.new("3","Hearts")
+   card_2 = Card.new("4", "Clubs")
+   deck = Deck.new([card_1, card_2])
+   round = Round.new(deck)
+   assert_instance_of Round, round
+  end
+
+
+  def test_it_takes_deck
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+  end
+
+  def test_guesses_starts_as_an_empty_array
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
+  end
+
+  def test_the_current_card
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
+
+end
