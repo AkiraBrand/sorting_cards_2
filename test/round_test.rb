3eec7a1e5aa9daeae0a6e60dda8c952efee16691
Josh Thompson <thompsonjoshd@gmail.com>
2018-08-20 15:56:25 -0600

    reducing cognitive overhead

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,53 +7,39 @@ require './lib/round'
 require 'pry'
 
 class RoundTest < Minitest::Test
-
+  def setup
+    @card_1 = Card.new("3","Hearts")
+    @card_2 = Card.new("4", "Clubs")
+    @deck = Deck.new([@card_1, @card_2])
+    @round = Round.new(@deck)
+  end
+  
   def test_it_exists
-   card_1 = Card.new("3","Hearts")
-   card_2 = Card.new("4", "Clubs")
-   deck = Deck.new([card_1, card_2])
-   round = Round.new(deck)
-   assert_instance_of Round, round
+   assert_instance_of Round, @round
   end
 
 
   def test_it_takes_deck
-    card_1 = Card.new("3","Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    assert_equal deck, round.deck
+    assert_equal @deck, @round.deck
   end
 
   def test_guesses_starts_as_an_empty_array
-    card_1 = Card.new("3","Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    assert_equal [], round.guesses
+    assert_equal [], @round.guesses
   end
 
   def test_the_current_card
-    card_1 = Card.new("3","Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    assert_equal card_1, round.current_card
+    assert_equal @card_1, @round.current_card
   end
 
   def test_it_records_current_guess
-    card_1 = Card.new("3","Hearts")
-    card_2 = Card.new("4", "Clubs")
-    deck = Deck.new([card_1, card_2])
-    round = Round.new(deck)
-    guess = Guess.new("3 of Hearts", card_1)
-    round.record_guess({value: "3", suit: "Hearts"})
+    guess = Guess.new("3 of Hearts", @card_1)
+    @round.record_guess({value: "3", suit: "Hearts"})
     # binding.pry
     # round.current_card
-    assert_equal 1, round.guesses.count
-    binding.pry
-    assert_includes round.guesses, guess
-    binding.pry
+    assert_equal 1, @round.guesses.count
+    # binding.pry
+    assert_includes @round.guesses, guess
+    # binding.pry
   end
 
 end
