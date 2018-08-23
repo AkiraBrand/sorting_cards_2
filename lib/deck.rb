258f1f876cb5621caf4cece1e51260a1f3a8768e
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-22 19:40:04 -0600

    look at sorting face value cards test

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,22 +12,8 @@ class Deck
     @cards.length
   end
 
-  # def suits
-  #   @suits = {Clubs: 1,
-  #            Diamonds: 2,
-  #            Hearts: 3,
-  #            Spades: 4}
-  # end
-
   def sort
-      #   (@cards.length-1).times do |index|
-      #   while @cards[index].value > @cards[index +1].value
-      #     @cards[index], @cards[index +1] = @cards[index +1], @cards[index]
-      #     # binding.pry
-      #   end
-      # end
-      # return @cards
-      # [index+1], @cards[index]
+
   end
 
   def sort_small_values(cards)
@@ -67,41 +53,6 @@ end
       while cards[index].value > cards[index+1].value
         cards[index], cards[index + 1] = cards[index + 1], cards[index]
       end
+      return cards
     end
   end
-  #
-  # def sort_based_on_suit(cards)
-  #   suits = {Clubs: 1,
-  #            Diamonds: 2,
-  #            Hearts: 3,
-  #            Spades: 4}
-  #
-  #   (@cards.count-1).times do |index|
-  #     while suits[cards[index].suit.to_sym] > suits[cards[index+1].suit.to_sym]
-  #       cards[index], cards[index + 1] = cards[index + 1], cards[index]
-  #     end
-  #     return cards
-  #   end
-  # end
-
-  # def card_values_as_strings
-  #   @cards.map do |card|
-  #     card.value
-  #   end
-  # end
-  #
-  # def card_values_as_integers
-  #   card_values_as_strings.map do |card|
-  #     card.to_i
-  #   end
-  # end
-  #
-  # def swap_the_values
-  #   card_values_a
-
-
-
-    # while @cards[0].value > @cards[1].value
-    #   @cards[0], @cards[1] = @cards[1], @cards[0]
-    #  return @cards[0], @cards[1]
-    # end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -35,7 +35,6 @@ def test_it_can_sort_one_card
   skip
   card_1 = Card.new("3", "Hearts")
   deck = Deck.new([card_1])
-  # binding.pry
   assert_equal [card_1], deck.sort
 end
 
@@ -47,12 +46,12 @@ def test_it_can_sort_two_cards
 end
 
 def test_it_can_sort_three_cards_based_on_value
+  skip
   card_1 = Card.new("5", "Hearts")
   card_2 = Card.new("3", "Clubs")
   card_3 = Card.new("8", "Spades")
   deck = Deck.new([card_1, card_2, card_3])
   assert_equal [card_2, card_1, card_3], deck.sort_small_values([card_1, card_2, card_3])
-  # binding.pry
 end
 
 def test_it_can_sort_two_cards_based_on_suit
@@ -71,24 +70,5 @@ def test_it_can_sort_face_value_cards
   result = deck.sort_face_values([card_1, card_2])
   assert_equal [card_2, card_1], result
 end
-
-
-
-# def test_it_can_extract_values
-#   card_1 = Card.new("5", "Hearts")
-#   card_2 = Card.new("3", "Clubs")
-#   card_3 = Card.new("8", "Spades")
-#   deck = Deck.new([card_1, card_2, card_3])
-#   assert_equal ["5", "3", "8"], deck.card_values_as_strings
-# end
-#
-# def test_it_can_turn_those_values_into_integers
-#   card_1 = Card.new("5", "Hearts")
-#   card_2 = Card.new("3", "Clubs")
-#   card_3 = Card.new("8", "Spades")
-#   deck = Deck.new([card_1, card_2, card_3])
-#   assert_equal [5, 3, 8], deck.card_values_as_integers
-#   binding.pry
-# end
 
 end
